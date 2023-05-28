d429507c18bd0326eec801bf44261fb47a7a94b3
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-05-28 22:39:23 +0900

    Update club7.py

--- club7.py
+++ club7.py
@@ -1,9 +1,10 @@
-def factorial(i):
+#팩토리얼값 함수
+def factorial(i):   #함수 만들기
 
     for a in range( 1, int(i) ):
 
-        i = int(i) + a
+        i = int(i) + a  #i에 a더하기
         
     return i
 
-print( factorial( input() ) )
+print( factorial( input() ) )   #입력, 출력
